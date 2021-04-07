f8c8b672a8b779ec591a30645b932b67fd121067
Nithin Urs <70891889+nithinurs46@users.noreply.github.com>
2021-04-07 22:12:50 +0530

    Added file to test reading of properties file

---
+++ DemoApp/src/main/java/com/demo/model/PropertiesReader.java
@@ -0,0 +1,43 @@
+package com.demo.model;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:someConfig.properties")
+public class PropertiesReader {
+
+	@Value("${key1}")
+	private String key1;
+
+	@Value("${key2}")
+	private String key2;
+
+	@Value("${key3}")
+	private String key3;
+
+	public String getKey1() {
+		return key1;
+	}
+
+	public void setKey1(String key1) {
+		this.key1 = key1;
+	}
+
+	public String getKey2() {
+		return key2;
+	}
+
+	public void setKey2(String key2) {
+		this.key2 = key2;
+	}
+
+	public String getKey3() {
+		return key3;
+	}
+
+	public void setKey3(String key3) {
+		this.key3 = key3;
+	}
+}
