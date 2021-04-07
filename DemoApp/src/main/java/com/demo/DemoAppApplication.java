07b4c85681b5af941b05f455b0aa292cb432af59
Nithin Urs <70891889+nithinurs46@users.noreply.github.com>
2021-04-07 22:13:40 +0530

    Reading properties file value

--- DemoApp/src/main/java/com/demo/DemoAppApplication.java
+++ DemoApp/src/main/java/com/demo/DemoAppApplication.java
@@ -1,13 +1,27 @@
 package com.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.demo.model.PropertiesReader;
+
 @SpringBootApplication
-public class DemoAppApplication {
+public class DemoAppApplication implements ApplicationRunner {
+
+	@Autowired
+	PropertiesReader reader;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAppApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("reading key 1:- " + reader.getKey1());
+
 	}
 
 }
